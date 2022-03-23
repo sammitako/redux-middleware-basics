7dd3cbdd6aae91a4015b7071013bb4b7b3055ad5
sammitako <p.hansaem@hotmail.com>
2022-03-23 22:09:04 +0900

    Checked logging state

--- src/middlewares/myLogger.js
+++ src/middlewares/myLogger.js
@@ -2,6 +2,7 @@ const myLogger = (store) => (next) => (action) => {
   console.log(action);
   // 컨테이너 컴포넌트에서 dispatch 됬을 때의 결과물
   const result = next(action);
+  console.log("\t", store.getState()); // 액션이 리듀서에서 모두 처리가 된 후의 상태
   return result;
 };
 
