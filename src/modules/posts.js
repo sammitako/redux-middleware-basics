1285bbddb8eb8d1e7d8cd28d2ee556e943128752
sammitako <p.hansaem@hotmail.com>
2022-03-24 18:54:05 +0900

    Refactoring: created reducerUtils

--- src/modules/posts.js
+++ src/modules/posts.js
@@ -1,4 +1,5 @@
 import * as postsAPI from "../api/posts";
+import { reducerUtils } from "../lib/asyncUtils";
 
 // API 요청 하나당 액션 3개씩
 const GET_POSTS = "GET_POSTS";
@@ -9,7 +10,7 @@ const GET_POST = "GET_POST";
 const GET_POST_SUCCESS = "GET_POST_SUCCESS";
 const GET_POST_ERROR = "GET_POST_ERROR";
 
-// thunk 생성 함수
+// thunk 생성 함수 -> dispatch는 리듀서의 action으로 들어감
 export const getPosts = () => async (dispatch) => {
   // 요청이 시작됨
   dispatch({ type: GET_POSTS });
@@ -25,12 +26,12 @@ export const getPosts = () => async (dispatch) => {
   }
 };
 
-export const getPost = () => async (dispatch) => {
+export const getPost = (id) => async (dispatch) => {
   // 요청이 시작됨
   dispatch({ type: GET_POST });
   // API를 호출
   try {
-    const post = await postsAPI.getPost();
+    const post = await postsAPI.getPostById(id);
     dispatch({ type: GET_POST_SUCCESS, post });
   } catch (e) {
     dispatch({
@@ -39,3 +40,47 @@ export const getPost = () => async (dispatch) => {
     });
   }
 };
+
+// 초기 상태
+const initialState = {
+  posts: reducerUtils.initial(),
+  post: reducerUtils.initial(),
+};
+
+// 리듀서
+export default function posts(state = initialState, action) {
+  switch (action.type) {
+    case GET_POSTS:
+      return {
+        ...state,
+        posts: reducerUtils.loading(),
+      };
+    case GET_POSTS_SUCCESS:
+      return {
+        ...state,
+        posts: reducerUtils.success(action.posts),
+      };
+    case GET_POSTS_ERROR:
+      return {
+        ...state,
+        posts: reducerUtils.error(action.error),
+      };
+    case GET_POST:
+      return {
+        ...state,
+        post: reducerUtils.loading(),
+      };
+    case GET_POST_SUCCESS:
+      return {
+        ...state,
+        post: reducerUtils.success(action.post),
+      };
+    case GET_POST_ERROR:
+      return {
+        ...state,
+        post: reducerUtils.error(action.error),
+      };
+    default:
+      return state;
+  }
+}
