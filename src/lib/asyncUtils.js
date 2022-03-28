880bc7d9d77a6b9a6bf872d00941d9ae22cdb531
sammitako <p.hansaem@hotmail.com>
2022-03-28 13:49:50 +0900

    Refactoring: createPromiseThunk

--- src/lib/asyncUtils.js
+++ src/lib/asyncUtils.js
@@ -1,3 +1,27 @@
+export const createPromiseThunk = (type, promiseCreator) => {
+  // Action Type
+  const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
+
+  // thunk 생성함수, 여기서 파라미터는 하나로 통일해서 나중에 비구조할당으로 사용
+  // Flux Standard Action
+  return (param) => async (dispatch) => {
+    dispatch({ type });
+    try {
+      const payload = await promiseCreator(param); // posts, post
+      dispatch({
+        type: SUCCESS,
+        payload,
+      });
+    } catch (e) {
+      dispatch({
+        type: ERROR,
+        payload: e,
+        error: true,
+      });
+    }
+  };
+};
+
 export const reducerUtils = {
   initial: (data = null) => ({
     data,
--- src/modules/posts.js
+++ src/modules/posts.js
@@ -1,5 +1,5 @@
 import * as postsAPI from "../api/posts";
-import { reducerUtils } from "../lib/asyncUtils";
+import { createPromiseThunk, reducerUtils } from "../lib/asyncUtils";
 
 // API 요청 하나당 액션 3개씩
 const GET_POSTS = "GET_POSTS";
@@ -10,36 +10,9 @@ const GET_POST = "GET_POST";
 const GET_POST_SUCCESS = "GET_POST_SUCCESS";
 const GET_POST_ERROR = "GET_POST_ERROR";
 
-// thunk 생성 함수 -> dispatch는 리듀서의 action으로 들어감
-export const getPosts = () => async (dispatch) => {
-  // 요청이 시작됨
-  dispatch({ type: GET_POSTS });
-  // API를 호출
-  try {
-    const posts = await postsAPI.getPosts();
-    dispatch({ type: GET_POSTS_SUCCESS, posts });
-  } catch (e) {
-    dispatch({
-      type: GET_POSTS_ERROR,
-      error: e,
-    });
-  }
-};
-
-export const getPost = (id) => async (dispatch) => {
-  // 요청이 시작됨
-  dispatch({ type: GET_POST });
-  // API를 호출
-  try {
-    const post = await postsAPI.getPostById(id);
-    dispatch({ type: GET_POST_SUCCESS, post });
-  } catch (e) {
-    dispatch({
-      type: GET_POST_ERROR,
-      error: e,
-    });
-  }
-};
+// actionType, promiseCreator
+export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
+export const getPost = createPromiseThunk(GET_POST, postsAPI.getPostById);
 
 // 초기 상태
 const initialState = {
@@ -58,12 +31,12 @@ export default function posts(state = initialState, action) {
     case GET_POSTS_SUCCESS:
       return {
         ...state,
-        posts: reducerUtils.success(action.posts),
+        posts: reducerUtils.success(action.payload),
       };
     case GET_POSTS_ERROR:
       return {
         ...state,
-        posts: reducerUtils.error(action.error),
+        posts: reducerUtils.error(action.payload),
       };
     case GET_POST:
       return {
@@ -73,12 +46,12 @@ export default function posts(state = initialState, action) {
     case GET_POST_SUCCESS:
       return {
         ...state,
-        post: reducerUtils.success(action.post),
+        post: reducerUtils.success(action.payload),
       };
     case GET_POST_ERROR:
       return {
         ...state,
-        post: reducerUtils.error(action.error),
+        post: reducerUtils.error(action.payload),
       };
     default:
       return state;
