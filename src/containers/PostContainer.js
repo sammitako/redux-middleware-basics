db3490c9dc1d6388bcfa0038f50aaa8ef4b731fc
sammitako <p.hansaem@hotmail.com>
2022-03-29 11:36:04 +0900

    Perf: getPostReducer

--- src/containers/PostContainer.js
+++ src/containers/PostContainer.js
@@ -1,22 +1,27 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import Post from "../components/Post";
+import { reducerUtils } from "../lib/asyncUtils";
 import { clearPost, getPost } from "../modules/posts";
 
 const PostContainer = ({ postId }) => {
-  const { data, loading, error } = useSelector((state) => state.posts.post);
+  const { data, loading, error } = useSelector(
+    (state) => state.posts.post[postId] || reducerUtils.initial()
+  );
   const dispatch = useDispatch();
 
   useEffect(() => {
+    // 데이터 있으면 API 요청 안함
+    if (data) return;
     dispatch(getPost(postId));
 
     // unmount, postId가 변경되서 useEffect 함수가 호출되기 직전
-    return () => {
-      dispatch(clearPost());
-    };
+    // return () => {
+    //   dispatch(clearPost());
+    // };
   }, [postId, dispatch]);
 
-  if (loading) return <div>Loading...</div>;
+  if (loading && !data) return <div>Loading...</div>;
   if (error) return <div>ERROR !</div>;
   if (!data) return null;
 
--- src/modules/posts.js
+++ src/modules/posts.js
@@ -16,19 +16,69 @@ const GET_POST_ERROR = "GET_POST_ERROR";
 
 const CLEAR_POST = "CLEAR_POST";
 
-// actionType, promiseCreator
+// thunk 함수 작성
+// (actionType, promiseCreator)
 export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
-export const getPost = createPromiseThunk(GET_POST, postsAPI.getPostById);
+// export const getPost = createPromiseThunk(GET_POST, postsAPI.getPostById);
+export const getPost = (id) => async (dispatch) => {
+  dispatch({ type: GET_POST, meta: id });
+  try {
+    // API 요청
+    const payload = await postsAPI.getPostById(id);
+    dispatch({ type: GET_POST_SUCCESS, payload, meta: id });
+  } catch (e) {
+    dispatch({
+      type: GET_POST_ERROR,
+      payload: e,
+      error: true,
+      meta: id,
+    });
+  }
+};
+
 export const clearPost = () => ({ type: CLEAR_POST });
 
 // 초기 상태
 const initialState = {
   posts: reducerUtils.initial(),
-  post: reducerUtils.initial(),
+  // post: reducerUtils.initial(),
+  post: {},
 };
 
 const getPostsReducer = handleAsyncActions(GET_POSTS, "posts", true);
-const getPostReducer = handleAsyncActions(GET_POST, "post");
+// const getPostReducer = handleAsyncActions(GET_POST, "post");
+const getPostReducer = (state, action) => {
+  const id = action.meta;
+  switch (action.type) {
+    case GET_POST:
+      return {
+        ...state,
+        post: {
+          ...state.post,
+          [id]: reducerUtils.loading(state.post[id] && state.post[id].data),
+        },
+      };
+    case GET_POST_SUCCESS:
+      return {
+        ...state,
+        post: {
+          ...state.post,
+          [id]: reducerUtils.success(action.payload),
+        },
+      };
+    case GET_POST_ERROR:
+      return {
+        ...state,
+        post: {
+          ...state.post,
+          [id]: reducerUtils.error(action.payload),
+        },
+      };
+    default:
+      return state;
+  }
+};
+
 // 리듀서
 export default function posts(state = initialState, action) {
   switch (action.type) {
